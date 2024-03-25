a3736d7d0534e891d1a761dc01af2737eab9e3e9
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-03-25 11:59:48 -0300

    Update cart in cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
-if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId:'1'
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId:'2'
-    },
-  ];
+export let cart;
+
+loadFromLocalStorage();
+
+function loadFromLocalStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
+  }
 }
 
 function saveToStorage() {
@@ -33,7 +39,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
-      deliveryOptionId: '1'
+      deliveryOptionId: "1",
     });
   }
 
@@ -52,7 +58,7 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 
-export function updateDeliveryOptions(productId,deliveryOptionId){
+export function updateDeliveryOptions(productId, deliveryOptionId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
